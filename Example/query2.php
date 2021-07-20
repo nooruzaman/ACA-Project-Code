51de3fcdee3be81c5a74b3f4c9b09262890a8f50
nooruzaman <35187802+nooruzaman@users.noreply.github.com>
2021-07-20 14:47:31 +1000

    Add files via upload

---
+++ Example/query2.php
@@ -0,0 +1,42 @@
+<html>
+     <body>
+          <?php
+               include 'get-parameters.php';
+			   echo $ep;
+			   echo $db;
+			   echo $un;
+			   echo $pw;
+               $conn = new mysqli($ep, $un, $pw, $db);
+               $_pick = $_POST['selection'];
+			   echo $_conn;
+			   echo "ABCD";
+			   echo $_pick;
+               switch ($_pick) {
+                    case "Q1":
+                    
+                         include 'mobile.php';
+                         break;
+                         
+                    case "Q2":
+                         include 'population.php';
+                         break;
+                         
+                    case "Q3":
+                         include 'lifeexpectancy.php';
+                         break;
+                         
+                    case "Q4":
+                         include 'gdp.php';
+                         break;
+                         
+                    case "Q5":
+                         include 'mortality.php';
+                         break;
+               }
+          ?>
+
+          <div id="Copyright" class="center">
+               <h5>&copy; 2020, Amazon Web Services, Inc. or its Affiliates. All rights reserved.</h5>
+          </div>
+     </body>
+</html>
